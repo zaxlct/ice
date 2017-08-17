bb9b756f3bda4afd441d0202946189b4e713c4b9
zaxlct <zaxlct@foxmail.com>
2017-08-17 22:07:00 +0800

    更正字段大小写

--- server/config/index.js
+++ server/config/index.js
@@ -3,6 +3,6 @@ export default {
   wechat: {
     token: 'mytoken',
     appID: 'wx5402eba5a5bc3884',
-    appsecret: '012751db0f2cb1db69daaf7d9b3559b5'
+    appSecret: '012751db0f2cb1db69daaf7d9b3559b5'
   }
 }
