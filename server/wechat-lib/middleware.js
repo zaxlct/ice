fd392898d2ad44345c3ab74cdd91edba93d8cbd3
zaxlct <zaxlct@foxmail.com>
2017-08-18 21:38:24 +0800

    重构变量/方法命名

--- server/wechat-lib/middleware.js
+++ server/wechat-lib/middleware.js
@@ -1,8 +1,9 @@
 import sha1 from 'sha1'
 import getRawBody from 'raw-body'
 import * as util from './util'
+import replyToWexinTemplate from './replyTpl'
 
-export default (opts, reply) => async (ctx, next) => {
+export default opts => async (ctx, next) => {
   const token = opts.token
   const {
     signature,
@@ -23,27 +24,22 @@ export default (opts, reply) => async (ctx, next) => {
   if (ctx.method === 'GET') {
     ctx.body = echostr
   } else if (ctx.method === 'POST') {
-    const data = await getRawBody(ctx.req, {
+    const receiveFromWeixinXML = await getRawBody(ctx.req, {
       length: ctx.length,
       limit: '1mb',
       encoding: ctx.charset
     })
 
     // 微信推送的原始数据包
-    const content = await util.xmlTojs(data)
-    ctx.weixin = util.formatMessage(content.xml)
+    const receiveFromWeixinJSON = await util.xmlTojs(receiveFromWeixinXML)
+    const receiveFromWeixinMessage = util.formatMessage(receiveFromWeixinJSON.xml)
 
-    await reply.apply(ctx, [ctx, next])
-
-    /*
-     * @ctx.weixin 接收到的微信推送的事件提醒 or 接收到的微信推送的普通消息，原本是 xml 格式，在这里是格式后的 JSON 格式
-     * @ctx.body 准备回复给微信用户的消息体
-     * ctx.weixin + ctx.body 组成回复给用户的 XML 结构体
-     */
-    const xml = util.tpl(ctx.body, ctx.weixin)
+    const replyToWexinTplData = await replyToWexinTemplate(receiveFromWeixinMessage)
+    const replyToWexinXML = util.dataToXmlTpl(receiveFromWeixinMessage, replyToWexinTplData)
+    console.log('replyToWexinXML=>', replyToWexinXML)
 
     ctx.status = 200
     ctx.type = 'application/xml'
-    ctx.body = xml
+    ctx.body = replyToWexinXML
   }
 }
