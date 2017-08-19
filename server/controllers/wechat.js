dfb15a587485de8f1f3ca43ae129b03fb8354de4
zaxlct <zaxlct@foxmail.com>
2017-08-19 19:16:44 +0800

    微信网页授权中间件

--- server/controllers/wechat.js
+++ server/controllers/wechat.js
@@ -1,13 +1,47 @@
 import * as api from '../api'
+import config from '../config'
+import { parse as urlParse } from 'url'
+import { parse as queryParse } from 'querystring'
 
 export const signature = async (ctx, next) => {
-  const url = ctx.query.url
+  let url = ctx.query.url
   if(!url) ctx.throw(404)
+  url = decodeURIComponent(url)
 
   const params = await api.getSignatureAsync(url)
 
   ctx.body = {
     success: true,
     params,
+  }
+}
+
+// 网页上点某按钮，直接跳转到 http://x.o/wechat-redirect?visit=a&id=b
+// 用户被重定向到 Wechat Redirect URL 授权验证
+// 验证后，自动二跳进入 http://x.o/oauth?code=xxxxxx&state=a_b
+export const redirect = async (ctx, next) => {
+  const target = config.SITE_ROOT_URL + '/oauth'
+  const scope = 'snsapi_userinfo'
+  const { a,b } = ctx.query
+  const params = a + '_' + b
+
+  const url = api.getAuthorizeURL(scope, target, params)
+  ctx.redirect(url)
+}
+
+export const oauth = async (ctx, next) => {
+  const url = ctx.query.url
+  const urlObj = urlParse(decodeURIComponent(url))
+  const params = queryParse(urlObj.query)
+  const code = params.code
+  const user = await api.getUserByCode(code)
+
+  console.log(user)
+  ctx.session = {
+    openid: user.openid
+  }
+  ctx.body = {
+    success: true,
+    user: user
   }
 }
--- server/middlewares/router.js
+++ server/middlewares/router.js
@@ -1,12 +1,14 @@
 import Router from 'koa-router'
 import wechatMiddle from '../wechat-lib/middleware'
 import config from '../config'
-import {signature} from '../controllers/wechat'
+import { signature, redirect, oauth } from '../controllers/wechat'
 
 export const router = app => {
   const router = new Router()
   router.all('/wechat-hear', wechatMiddle(config.wechat))
   router.get('/wechat-signature', signature)
+  router.get('/wechat-redirect', redirect) // 访问这个 URL 会重定向到微信的官方授权页面，这个 URL 上可以加一下query 参数
+  router.get('/wechat-oauth', oauth) // 在微信官方授权页面点击确定后，回到这个 URL，此时这个 URL 会有微信的 code，和上面定义的 query
 
   app.use(router.routes()).use(router.allowedMethods())
 }
